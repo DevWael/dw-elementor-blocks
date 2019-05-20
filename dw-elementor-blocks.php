9784f5625f466b6aa94bb09d0356fb56fa4c2740
Ahmad Wael <dev.ahmedwael@gmail.com>
2019-05-20 14:11:48 +0200

    first commit

---
+++ dw-elementor-blocks.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Plugin Name: DW Elementor Blocks
+ * Plugin URI: https://github.com/DevWael/dw-elementor-blocks
+ * Description: An Elementor toolkit that adds a new elementor widgets.
+ * Version: 0.1
+ * Author: Ahmad Wael
+ * Author URI: https://bbioon.com
+ * Text Domain: dw
+ * Domain Path: /languages/
+ *
+ * @package dw
+ */
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly
+}
+
+
